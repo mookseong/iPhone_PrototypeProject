aff0cfb16c678bb279e3f60cdefc4893ce031ac4
limseongmuk <kmkook12312@gmail.com>
2022-05-29 15:27:21 +0900

    trade_in 프로토타입

---
+++ product/trade_in.js
@@ -0,0 +1,12 @@
+const Trade_in = {
+    user_model: this.user_model,
+    user_storage: this.user_storage,
+    user_state: function () {
+        let  test= state_info
+    }
+};
+
+const d = {
+    user_button : this.user_button,
+    user_screen : this.user_screen
+};
