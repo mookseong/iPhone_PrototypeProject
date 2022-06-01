d0a60291f1a165f29f6e8a275e9fa6bdc6a1a100
limseongmuk <kmkook12312@gmail.com>
2022-06-01 23:33:48 +0900

    + 구매기능 개선

--- Function/buy/BuyEngine.js
+++ Function/buy/BuyEngine.js
@@ -1,34 +1,43 @@
+
+
 export const BuySystem = (function () {
     function BuySystem() {
     }
 
-    BuySystem.prototype.execute = function (commander) {
-        commander.execute();
-        commander.buy();
+    BuySystem.prototype.select = function (context) {
+        context.execute();
     };
+
+    BuySystem.prototype.buy = function (context) {
+        context.buy();
+    }
 
     return BuySystem;
 })();
 
 export const BuyContext = (function () {
     function ListSetup() {
+        this.BuyContext = null
         this.commands = [];
+
     }
 
     ListSetup.prototype.buy = function () {
         console.log("(선택한 옵션 보여준다)")
         console.log("1.구매하기\t2.장바구니 추가")
+
     };
 
     ListSetup.prototype.execute = function () {
-        this.commands.forEach(function (command) {
-            command();
+        this.commands.forEach(function (context) {
+            context();
+
         });
     };
 
-    ListSetup.prototype.option = function (command, args) {
+    ListSetup.prototype.option = function (context, args) {
         this.commands.push(function () {
-            command.call(null, args);
+            context.call(null, args);
         });
     };
 
