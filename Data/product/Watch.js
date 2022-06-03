e5e66edd1619951ce76b15bf8f6dfeb4adf82955
Donghwi Kim <ehdgnl0904@naver.com>
2022-06-03 14:19:03 +0900

    Watch 내용 수정

--- Data/product/Watch.js
+++ Data/product/Watch.js
@@ -1,7 +1,25 @@
-// AppleWatch에 plusCellular 처리. Series3은 해당 안 됨
-class AppleWatch {
-    constructor(name, size) {
-        this.name = name;
-        this.size = size;
+import {Watch} from "../../Model/DataModel.js";
+
+const Watch7 =
+    Watch("Watch7").setModel("A1346").setGeneration(7).setColor('Silver').build();
+
+export const Watch_Option = {
+    WatchModel: function () {
+        console.log("원하는 모델을 선택하세요.");
+        console.log('1.41mm형 디스플레이 - ₩559,000부터\n2.45mm형 디스플레이 - ₩599,000부터\n');
+    },
+    WatchColor: function () {
+        console.log("맘에 드는 색상을 선택하세요.");
+        console.log('1.Space Gray\t2.Silver\n');
+    },
+    Cellular: function () {
+        console.log("원하시는 연결성을 선택해주세요");
+        console.log('1.WIFI \t2.WIFI + Cellular\n');
+    },
+    trade_in: function () {
+        console.log('보상 판매할 AppleWatch가 있으신가요?\n');
     }
-}
+};
+
+//출력 테스트
+console.log(Watch7);
