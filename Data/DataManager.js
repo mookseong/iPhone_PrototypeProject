84c2d29cb912e3471ed42703a4645c78ae1bd75b
limseongmuk <kmkook12312@gmail.com>
2022-06-05 00:30:54 +0900

    + 코드수정

--- Data/DataManager.js
+++ Data/DataManager.js
@@ -1,21 +1,11 @@
 import { Basket } from "../Function/basket/Basket.js";
+import {iModel} from "../Model/DataModel.js";
 
 // 제품 등록, 제품 삭제, 리스트 보여주기
 export const DataManager = (function() {
-    var item;   // var 키워드로 블록 스코프를 만들어 item에 외부에서 item에 접근할 수 없게 함
-
     function DataManager(data) {
         item = data ? data : '';
         this.dataList = [];
-    };
-    // get, set메서드를 사용하여 사용자가 item에 직접 접근할 수 없게 함.(캡슐화)
-    DataManager.prototype = {
-        getData : function() {
-            return item;
-        },
-        setData : function(data) {
-            item = data;
-        }
     };
     DataManager.prototype.register = function(item) {
         this.dataList.push(item);
@@ -41,7 +31,7 @@ export const DataManager = (function() {
 const Product = (function() {
     function Product(name) {
         this.name = name;
-    };
+    }
     Product.prototype.update = function() {
         console.log(`${this.name}이 장바구니에 있습니다.`)
     }
@@ -49,48 +39,4 @@ const Product = (function() {
 })();
 
 
-// testcode
-const datamanager = new DataManager;
 
-const iPhone = new Product('iPhone');
-const iPad = new Product('iPad');
-const Airpods = new Product('Airpods');
-
-datamanager.register(iPhone);
-datamanager.register(iPad);
-datamanager.register(Airpods);
-console.log(datamanager.getDataList());     // 항목을 출력하고 마지막에 undefined 까지 출력함...
-
-datamanager.unregister(iPhone);
-console.log(datamanager.getDataList());
-
-
-/* 수량 조졀 + notifyAll 코드
-DataManager.prototype.plusAmount = function(item) {
-    this.dataList.some(function(iName) {
-        console.log(iName.item, item, iName.item === item);
-        if (iName.item === item) {
-            ++iName.amount;
-            return true;
-        }
-    });
-};
-DataManager.prototype.minusAmount = function(item) {
-    this.dataList.some(function(iName) {
-        console.log(iName.item, item, iName.item === item);
-        if (iName.item === item) {
-            --iName.amount;
-            return true;
-        }
-    });
-};
-DataManager.prototype.notifyAll = function() {
-    this.dataList.forEach((allData) => {
-        try {
-            allData.update(dataList);
-        } catch {
-            console.log("물건이 없습니다.");
-        }
-    });
-};
-*/
