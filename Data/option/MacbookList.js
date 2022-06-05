6689c67079771121de7edaa8d6eacdf03fd79531
Donghwi Kim <ehdgnl0904@naver.com>
2022-06-06 03:49:21 +0900

    + 소스코드 수정

--- Data/option/MacbookList.js
+++ Data/option/MacbookList.js
@@ -1,6 +1,6 @@
 import {Mac_Option} from "../Product/mac/MacOption.js";
 
-export const List_MacBookAir= function (commander) {
+export const List_MacBook= function (commander) {
     commander.option(Mac_Option.MacRam);
     commander.option(Mac_Option.MacColor);
     commander.option(Mac_Option.MacStorage);
