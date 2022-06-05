ff36e9971a327ec9c7450a4f93c940cf0383e45b
Donghwi Kim <ehdgnl0904@naver.com>
2022-06-06 03:50:19 +0900

    + 소스코드 수정

--- Data/option/AirpodList.js
+++ Data/option/AirpodList.js
@@ -1,7 +1,4 @@
-import {iPhone13_Option} from "../Product/iPhone.js"
-import {iPad_Pro_Option} from "../Product/iPad.js";
 import {AirPod_Option} from "../Product/AirPods.js";
-import {Mac_Option} from "../Product/mac/MacOption.js";
 
 
 export const List_AirPods= function(commander) {
