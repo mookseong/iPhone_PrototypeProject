8748c23566a8e4b745b6a37fc2bac5a48e660534
limseongmuk <kmkook12312@gmail.com>
2022-06-07 09:26:21 +0900

    + 주석 작성 및 AppleStore.js작성

--- data/DataManager.js
+++ data/DataManager.js
@@ -1,18 +1,24 @@
 // 제품 등록, 제품 삭제, 리스트 보여주기
+// 데이터매니저 기능 : 제품 등록, 제품 삭제, 리스트 보여주기
+// 제품 등록(register) 수행 시 DataManager에서 관리하는 데이터들을 모아놓은 배열(dataList)에 객체를 추가
+// 제품 삭제(unregister) 수행 시 dataList에서 삭제하고자 하는 객체를 제거한 배열을 반환
+// 리스트 보여주기(getDataList) : dataList의 모든 요소를 순회하며 출력해줌
 export const DataManager = (function () {
     function DataManager() {
         this.dataList = [];
     }
-
+    // 제품을 장바구니로 등록할 시 register를 사용하여 dataList에 등록함
     DataManager.prototype.register = function (item) {
         this.dataList.push(item);
-    }
+    };
     // 장바구니에서 deleteItem이 실행되면 dataList에서 item을 찾아 항목을 삭제함
     DataManager.prototype.unregister = function (item) {
         this.dataList = this.dataList.filter((iName) => iName !== item);
         console.log("항목을 삭제했습니다.");
     };
     // 장바구니에서 showList가 실행되면 dataList를 출력해줌
+    // 장바구니에서 deleteItem이 실행되면 dataList에서 item을 찾아 항목을 삭제함
+    // basket.deleteItem(item) --> unregister(item)
     DataManager.prototype.getDataList = function () {
         this.dataList.forEach((item) => {
             try {
@@ -22,6 +28,7 @@ export const DataManager = (function () {
             }
         })
     }
+
     return DataManager;
 }());
 
--- function/basket/Basket.js
+++ function/basket/Basket.js
@@ -1,10 +1,13 @@
-import { DataManager } from "../../Data/DataManager.js";
-
-// 제품 삭제, 리스트 보여주기
+// 장바구니 기능 : 제품 삭제, 리스트 보여주기
+// 제품 삭제 기능(deleteItem) 수행 시 DataManager의 unregister 함수를 호출
+// 장바구니(Basket)에서 데이터 삭제를 요청하고 데이터에 접근해서 삭제하는 작업은 DataManager에게 위임
+// 리스트 보여주기(showList) 수행 시 DataManager의 getDataList 함수를 호출하여 DataManager의 데이터를 불러옴
+// 사용자의 직접 접근을 basket으로 제한하기 위함
 export const Basket = (function() {
     function Basket() {
         this.datamanager = null
     }
+    // 특정 제품을 장바구니에서 삭제하고자 할 때
     Basket.prototype.setManager = function (manager){
         this.datamanager = manager
     }
@@ -12,6 +15,7 @@ export const Basket = (function() {
     Basket.prototype.deleteItem = function(item) {
         this.datamanager.unregister(item);
     };
+    // 장바구니에 있는 모든 제품을 보고싶을 때
     Basket.prototype.showList = function() {
         return this.datamanager.getDataList();
     }
